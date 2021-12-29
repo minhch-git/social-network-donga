04807914aecdc1b7cd1e0b3a8982c5bd3e2e01b3
minhchiu <minhch.vn@gmail.com>
2021-12-29 18:50:18 +0700

    [minhch] create timeDifference function

---
+++ src/public/js/common.js
@@ -0,0 +1,32 @@
+**
+ * Convert a Date into a Human-Readable
+ * @param {data} current date
+ * @param {date} previous date
+ * @returns time + string
+ */
+const timeDifference = (current, previous) => {
+  let msPerMinute = 60 * 1000
+  let msPerHour = msPerMinute * 60
+  let msPerDay = msPerHour * 24
+  let msPerMonth = msPerDay * 30
+  let msPerYear = msPerDay * 365
+  let elapsed = current - previous
+
+  if (elapsed < msPerMinute) {
+    if (elapsed / 1000 < 30) return 'Vừa xong'
+    return Math.round(elapsed / 1000) + ' giây trước'
+  }
+  if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + ' phút trước'
+  }
+  if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + ' giờ trước'
+  }
+  if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + ' ngày trước'
+  }
+  if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + ' tháng trước'
+  }
+  return Math.round(elapsed / msPerYear) + ' năm trước'
+}
