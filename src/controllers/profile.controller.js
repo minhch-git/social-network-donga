c6dddaf41ddb5200bebd66e764dc9c0ca33b3fbc
sonht113 <trongson18101999@gmail.com>
2021-12-29 18:56:15 +0700

    [hotrongson] edit profile controller

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,8 +1,13 @@
 import createError from 'http-errors'
 import pick from '../utils/pick'
 import catchAsync from '../utils/catchAsync'
-import { postService, userService } from '../services'
-import { tranSuccess } from '../../lang/en'
+import {
+  postService,
+  userService
+} from '../services'
+import {
+  tranSuccess
+} from '../../lang/en'
 import User from '../models/user.model'
 
 // Profile page
@@ -50,52 +55,7 @@ const getProfile = async (req, res) => {
     res.redirect('/not-found')
   }
 }
-/**
- * Create a post
- * @POST profile/:username/replies
- * @access private
- */
-const getReplies = async (req, res) => {
-  try {
-    const payload = await getProfilePayload(req.params.username, req.user)
-    payload['selectedTab'] = 'replies'
-    res.render('profile', {
-      ...payload,
-      errors: req.flash('errors'),
-      success: req.flash('success'),
-    })
-  } catch (error) {
-    res.redirect('/not-found')
-  }
-}
-/**
- * Create a post
- * @POST profile/:username
- * @access private
- */
-const getProfileByUsername = async (req, res) => {
-  try {
-    const payload = await getProfilePayload(req.params.username, req.user)
-    res.render('profile', {
-      ...payload,
-      errors: req.flash('errors'),
-      success: req.flash('success'),
-    })
-  } catch (error) {
-    res.redirect('/not-found')
-  }
-}
 
-// [GET] /profile/:username/following
-const getFollowing = async (req, res) => {
-  try {
-    let payload = await getProfilePayload(req.params.username, req.user)
-    payload['selectedTab'] = 'following'
-    return res.status(200).render('followers-following', payload)
-  } catch (error) {
-    res.redirect('/not-found')
-  }
-}
 
 // [GET] /profile/:username/followers
 const getFollowers = async (req, res) => {
