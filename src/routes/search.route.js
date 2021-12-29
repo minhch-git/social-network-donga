ea3e234f028fefb68cc64194bfe1de1d50f05be7
tonlongchau <tonlongchau@gmail.com>
2021-12-29 19:31:11 +0700

    [chautl] init search

--- src/routes/search.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { requireLoggedIn } from '../middlewares/auth'
-import { searchController } from '../controllers'
-const router = new Router()
-
-router.get('/:selectedTab', requireLoggedIn, searchController.getSearch)
-
-router.get('/', requireLoggedIn, searchController.getSearchPage)
-export default router
